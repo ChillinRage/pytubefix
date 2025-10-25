55442667ea14e59966ede9c361b0e66355581767
Chan How <chanhow1@gmail.com>
2025-10-26 00:06:14 +0800

    Import safe_filename in streams.py

--- pytubefix/streams.py
+++ pytubefix/streams.py
@@ -19,7 +19,7 @@ from urllib.parse import parse_qs
 from pathlib import Path
 
 from pytubefix import extract, request
-from pytubefix.helpers import target_directory
+from pytubefix.helpers import safe_filename, target_directory
 from pytubefix.itags import get_format_profile
 from pytubefix.monostate import Monostate
 from pytubefix.file_system import file_system_verify
